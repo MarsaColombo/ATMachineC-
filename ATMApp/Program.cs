e2226ce9cf80f15f8cd249628be3f46c8ee4a5c5
MarsaColombo <timbiaou@gmail.com>
2023-11-28 16:44:47 +0100

    Initial Push

---
+++ ATMApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class cardHolder
+{
+    String cardNum;
+    int pin;
+    String firstName;
+    String lastName;
+    double balance;
+
+    //Defining    
+    public cardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
+    {
+        this.cardNum = cardNum;
+        this.pin = pin;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+
+    }
+
+    //Getters Methods
+    public String getNum()
+    {
+        return cardNum;
+    }
+    public int getPin()
+    {
+        return pin;
+    }
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public String getLastName()
+    {
+        return lastName;
+    }
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    //Setters Methods
+
+}
